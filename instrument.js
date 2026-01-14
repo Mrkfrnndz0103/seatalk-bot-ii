965e0d78d239d79f93ecbfc5a822bf396bbfda84
Mark <romark.fernandez@spxexpress.comm>
2026-01-14 20:43:52 +0800

    sentry

--- instrument.js
+++ instrument.js
@@ -2,8 +2,8 @@
 const Sentry = require("@sentry/node");
 
 Sentry.init({
-  dsn: "https://6871948ee527f04a73c72e1e717bdbdd@o4510708673085440.ingest.us.sentry.io/4510708690845696",
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
+  dsn: process.env.SENTRY_DSN,
+  release: process.env.SENTRY_RELEASE,
+  tracesSampleRate: 1.0,
   sendDefaultPii: true,
 });
